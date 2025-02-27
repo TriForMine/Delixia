eb6f4b5e987984c6fd2c7dfe4844091abb9cce70
TriForMine <TriForMine@users.noreply.github.com>
2025-02-27 10:32:34 +0000

    deploy: e74cbf225ab60ecf02e2de2b48ba54a9ff2edc71

---
+++ assets/kernelBlurVaryingDeclaration-DfHK39Ti.js
@@ -0,0 +1,1 @@
+import{g as r}from"./index-DfJDJLmV.js";const e="kernelBlurVaryingDeclaration",a="varying vec2 sampleCoord{X};";r.IncludesShadersStore[e]=a;
---
+++ assets/mainUVVaryingDeclaration-C8So-KCX.js
@@ -0,0 +1,4 @@
+import{g as a}from"./index-DfJDJLmV.js";const e="mainUVVaryingDeclaration",n=`#ifdef MAINUV{X}
+varying vMainUV{X}: vec2f;
+#endif
+`;a.IncludesShadersStoreWGSL[e]=n;
