6c8cf87956faf9742ce960741786cd009b8dd810
TriForMine <TriForMine@users.noreply.github.com>
2025-03-30 17:47:00 +0000

    deploy: f55090b8de2775c7927435b4c32c55f3b7d70f91

---
+++ assets/hdrFiltering.fragment-BuuM0C1M.js
@@ -0,0 +1,5 @@
+import{S as e}from"./index-Da7UxN6R.js";import"./hdrFilteringFunctions-DTSw9LFd.js";const r="hdrFilteringPixelShader",i=`#include<helperFunctions>
+#include<importanceSampling>
+#include<pbrBRDFFunctions>
+#include<hdrFilteringFunctions>
+uniform float alphaG;uniform samplerCube inputTexture;uniform vec2 vFilteringInfo;uniform float hdrScale;varying vec3 direction;void main() {vec3 color=radiance(alphaG,inputTexture,direction,vFilteringInfo);gl_FragColor=vec4(color*hdrScale,1.0);}`;e.ShadersStore[r]||(e.ShadersStore[r]=i);const t={name:r,shader:i};export{t as hdrFilteringPixelShader};
