27a88ce947120adc38caeb28f4ade39487669681
TriForMine <TriForMine@users.noreply.github.com>
2025-03-30 09:23:48 +0000

    deploy: d8cb0584f96055f56f2e68c1e292339944259696

---
+++ assets/flowGraphPauseAnimationBlock-CGSdtqdJ.js
@@ -0,0 +1,1 @@
+import{l as t,R as e,d as i}from"./index-OR8pD1Ut.js";class o extends t{constructor(a){super(a),this.animationToPause=this.registerDataInput("animationToPause",e)}_execute(a){this.animationToPause.getValue(a).pause(),this.out._activateSignal(a)}getClassName(){return"FlowGraphPauseAnimationBlock"}}i("FlowGraphPauseAnimationBlock",o);export{o as FlowGraphPauseAnimationBlock};
--- assets/kernelBlurVaryingDeclaration-nDObXauZ.js
+++
@@ -1,1 +0,0 @@
-import{S as e}from"./index-BS2l1FY-.js";const r="kernelBlurVaryingDeclaration",a="varying sampleCoord{X}: vec2f;";e.IncludesShadersStoreWGSL[r]||(e.IncludesShadersStoreWGSL[r]=a);
